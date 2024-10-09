2d2c6ea774f9346de8f6adaa45564c8714fdb8b9
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-10-09 16:13:03 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,4 +1,11 @@
 function fib(n) {
-    fib(n) = fib(n-1)+ fib(n-2)
-    return n;
+    if (n === 0){
+        return 0;
+    }
+    else if (n ===1){
+        return 1;
+    else {
+         fib(n) = fib(n-1)+ fib(n-2)
+        return n;
+    }
 }
