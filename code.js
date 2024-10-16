08f0626a908f54567444490000a61516d01d128f
aerick-321 <158105735+aerick-321@users.noreply.github.com>
2024-10-16 15:38:42 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,12 +1,14 @@
 function fib(n) {
-    const arr = new Array [n + 1];
+    const arr = new Array (n + 1);
+    arr[0] = 0
+    arr[1] = 1
     
     function helpFib(x){
-        if (x === 0){
-            arr [x] = 0;
+        if (x===0){
+            return arr [x];
         }
         else if (x ===1){
-            arr [x]= 1;
+            return arr [x];
         }
         else {
             arr[x] = helpFib(x-1) + helpFib(x-2);
